73db6f270b8686b54918120c94c884b9b9d1fc91
Erwin-Cino <ErwinCino0812@gmail.com>
2019-10-16 23:20:49 +0800

    Adding education to profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -284,30 +284,28 @@ router.post(
 
     try {
       let profileExp = await Profile.findOne({
+        //experience: { company: "Test title." }
         user: req.user.id
       });
 
-      // let searchId = await Profile.findOne({
-      //   profileExp: { experience: { id: req.body.id } }
-      // });
-
-      //console.log(profileExp);
-      //console.log(searchId);
-
-      //experience: { id: id } null
-      //experience: { $elemMatch: { id: req.body.id } } null
-      //{ user: req.user.id }
-
-      //console.log({ searchId });
+      console.log(profileExp);
 
       if (profileExp) {
         // Update Profile
         profileExp = await Profile.findOneAndUpdate(
-          //{ profileExp: { experience: { id: req.body.id } } },
-          { user: req.user.id },
+          { experience: { "experience._id": req.body.id } },
+          //{ experience: { id: req.body.id } },
           {
             $set: {
-              experience: profileExperience
+              experience: {
+                title: profileExperience.title,
+                company: profileExperience.title,
+                location: profileExperience.location,
+                from: profileExperience.from,
+                to: profileExperience.to,
+                current: profileExperience.current,
+                description: profileExperience.description
+              }
             }
           },
           { new: true }
@@ -343,5 +341,69 @@ router.delete("/experience/:exp_id", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+// @route   PUT api/profile/education
+// @desc    Add profile education
+// @access  Private
+
+router.put(
+  "/education",
+  [
+    auth,
+    [
+      check("school", "School is required")
+        .not()
+        .isEmpty(),
+      check("degree", "Degree is required")
+        .not()
+        .isEmpty(),
+      check("fieldofstudy", "Field of study is required")
+        .not()
+        .isEmpty(),
+      check("from", "From date is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
+
+    const newEdu = {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.education.unshift(newEdu);
+
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
