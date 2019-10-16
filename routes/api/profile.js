a9fcf6e1c632233f4a00ba7c366da8e33f2b2c23
Erwin-Cino <ErwinCino0812@gmail.com>
2019-10-16 22:41:43 +0800

    Update experiences in Profile by ID using Post request

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -287,12 +287,12 @@ router.post(
         user: req.user.id
       });
 
-      let searchId = await Profile.findOne({
-        profileExp: { experience: { id: req.body.id } }
-      });
+      // let searchId = await Profile.findOne({
+      //   profileExp: { experience: { id: req.body.id } }
+      // });
 
       //console.log(profileExp);
-      console.log(profileExp);
+      //console.log(searchId);
 
       //experience: { id: id } null
       //experience: { $elemMatch: { id: req.body.id } } null
@@ -305,7 +305,11 @@ router.post(
         profileExp = await Profile.findOneAndUpdate(
           //{ profileExp: { experience: { id: req.body.id } } },
           { user: req.user.id },
-          { $set: profileExperience },
+          {
+            $set: {
+              experience: profileExperience
+            }
+          },
           { new: true }
         );
 
@@ -317,5 +321,27 @@ router.post(
     }
   }
 );
+
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+
+router.delete("/experience/:exp_id", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    // Get remove index
+    const removeIndex = profile.experience
+      .map(item => item.id)
+      .indexOf(req.params.exp_id);
+
+    profile.experience.splice(removeIndex, 1);
+    await profile.save();
+    res.json(profile);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
