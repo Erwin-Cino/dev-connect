2397e941b72f8167f3902e558167012e72b0c915
Erwin-Cino <ErwinCino0812@gmail.com>
2019-10-17 09:37:58 +0800

    Add and delete education in profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -406,4 +406,26 @@ router.put(
   }
 );
 
+// @route   DELETE api/profile/education/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+
+router.delete("/education/:edu_id", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    // Get remove index
+    const removeIndex = profile.education
+      .map(item => item.id)
+      .indexOf(req.params.edu_id);
+
+    profile.education.splice(removeIndex, 1);
+    await profile.save();
+    res.json(profile);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
