2043844bf59df8c6454ad4b848285a2709b4e3ba
Erwin-Cino <ErwinCino0812@gmail.com>
2019-10-17 13:52:27 +0800

    Like posts route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -91,7 +91,7 @@ router.delete("/:id", auth, async (req, res) => {
     }
 
     //Check if the user owns the post
-    if (post.user.toString !== req.user.id) {
+    if (post.user.toString() !== req.user.id) {
       return res.status(401).json({ msg: "User not authorized" });
     } else {
       await post.remove();
@@ -103,6 +103,28 @@ router.delete("/:id", auth, async (req, res) => {
     if (err.kind === "ObjectId") {
       return res.status(404).json({ msg: "Post not found" });
     }
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route   Put api/posts/like/:id
+// @desc    Like a post
+// @access  Private
+router.put("/like/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    //Check if post has already been liked by user
+    if (
+      post.likes.filter(like => like.user.toString() == req.user.id).length > 0
+    ) {
+      return res.status(400).json({ msg: "Post already liked" });
+    }
+    post.likes.unshift({ user: req.user.id });
+    await post.save();
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
     res.status(500).send("Server Error");
   }
 });
