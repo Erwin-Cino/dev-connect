3bf44cca8c1d589098071b130285d3473d56fa19
Erwin-Cino <ErwinCino0812@gmail.com>
2019-10-22 15:36:25 +0800

    Protected Route

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import Register from "./components/auth/Register";
 import Alert from "./components/layout/Alert";
 import { loadUser } from "./actions/auth";
 import setAuthToken from "./utils/setAuthToken";
+import Dashboard from "./components/dashboard/Dashboard";
+import PrivateRoute from "./components/routing/PrivateRoute";
 
 import "./App.css";
 
@@ -34,6 +36,7 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
